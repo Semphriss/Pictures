b9edab836cd881c6eedbd24844d7aaafacdad8d7
Semphris <semphris@protonmail.com>
2021-09-29 23:54:36 -0400

    Added GNU GPLv3+ boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,18 @@
+//  Pictures - A standard-looking, extensible and portable photo manager
+//  Copyright (C) 2021 Semphris <semphris@protonmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #if defined(unix) || defined(__unix__) || defined(__unix)
 #include <dirent.h>
